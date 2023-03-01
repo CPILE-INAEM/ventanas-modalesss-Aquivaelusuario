257c43c07a1f6641c6a84c554b72fe6ab7809f38
Aquivaelusuario <eframend@correo.com>
2023-03-01 13:19:00 +0100

    cambios añadidos

---
+++ script.js
@@ -0,0 +1,25 @@
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+
+console.log(btnShowModal)
+console.log(btnCloseModal)
+console.log(modal)
+console.log(overlay)
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  };
+
+  
+  const closeModal = function () {
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden');
+    };
+    
+    btnShowModal.forEach((btn) => btn.addEventListener('click', openModal))
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
